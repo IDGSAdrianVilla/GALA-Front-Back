349ba493b22fa92c73dd8a46f2f5edd878d9ce7b
yessicsa <jessipandita2616@gmail.com>
2023-01-09 19:40:21 -0600

    finalizacionde la estructura del proyecto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './gala/home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'gala', component: HomeComponent}
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
