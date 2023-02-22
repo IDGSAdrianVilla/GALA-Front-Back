f322463c6a9e2808ffd90ba5b3578b2321dfce21
Adrian Villa <idgs.adrian.villa@gmail.com>
2023-02-21 19:10:38 -0600

    Se agrega funcionalidad y consumo de multiselect para módulo de consulta usuarios

--- frontend/src/app/gala/home/modules/usuarios/usuarios-consulta/usuarios-consulta.component.ts
+++ frontend/src/app/gala/home/modules/usuarios/usuarios-consulta/usuarios-consulta.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MensajesService } from '../../../../../services/mensajes/mensajes.service';
 
 @Component({
   selector: 'app-usuarios-consulta',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuarios-consulta.component.css']
 })
 export class UsuariosConsultaComponent {
+  opcionesMultiselect : any = [];
+  
+  opcionesSeleccionadas: string[] = [];
+  tituloSelect = '';
+  mostrarOpciones = false;
 
+  constructor (
+    private mensajes : MensajesService
+  ) {
+
+  }
+
+  activaLista() {
+    this.mostrarOpciones = !this.mostrarOpciones;
+  }
+
+  seleccionarOpcion(option: string) {
+    const index = this.opcionesSeleccionadas.indexOf(option);
+    if (index === -1) {
+      this.opcionesSeleccionadas.push(option);
+    } else {
+      this.opcionesSeleccionadas.splice(index, 1);
+    }
+
+    this.tituloSelect = this.opcionesMultiselect.filter((opcion : any) => this.opcionesSeleccionadas.includes(opcion.pk)).map((opcion : any) => opcion.nombrePob).join(', ');
+  }
+
+  consultarPorRoles() : void {
+    if ( this.opcionesSeleccionadas.length > 0 ) {
+      // codigo de consulta back-end
+    } else {
+      this.mensajes.mensajeGenerico('Para consultar antes debes seleccionar algún Rol', 'info');
+    }
+  }
 }
