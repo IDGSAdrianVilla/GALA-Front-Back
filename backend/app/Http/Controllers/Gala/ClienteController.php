22c18824dcf3a17f39b895bef73438727924b1fa
Fabi <jessipandita2616@gmail.com>
2023-03-05 22:08:53 -0600

    Entrega estable módulo registro de clientes

---
+++ backend/app/Http/Controllers/Gala/ClienteController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Gala;
+
+use App\Http\Controllers\Controller;
+use App\Services\Gala\ClienteService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ClienteController extends Controller
+{
+    protected $clienteService;
+
+    public function __construct(
+        ClienteService $ClienteService
+    )
+    {
+        $this->clienteService = $ClienteService;
+    }
+
+    public function crearNuevoCliente( Request $request ){
+        try{
+            log::alert($request);
+            $this->clienteService->holi();
+            return $this->clienteService->crearNuevoCliente($request->all());
+
+        } catch( \Exception $error) {
+            Log::alert($error);
+            return response()->json(
+                [
+                    'error' => $error,
+                    'mensaje' => 'Ocurrió un error al consultar'
+                ],
+                500
+            );
+        }
+
+    }
+}
